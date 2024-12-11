082d722223b29b253cc8d938a17fec1ef9c3d15e
manhdungusearchbtw <manhdungcuber@gmail.com>
2024-12-11 22:08:57 +0700

    Add files via upload
    i3, polybar,...

---
+++ dotfiles/README.md
@@ -0,0 +1,5 @@
+# 🍚 preview
+
+![image](https://github.com/user-attachments/assets/4f8c8d34-1f9c-4083-8970-8015cbf633d3)
+
+![image](https://github.com/user-attachments/assets/ada5875c-5607-4082-bfb9-e09bdf02e487)
---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,49 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 999
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+  pattern = "*",
+  callback = function()
+    require("conform").format { async = false }
+  end,
+})
